10170ed5740b8902028a1dac7cbb590f84ca291b
nicosanji <n.sangiorgio92@gmail.com>
2022-03-03 22:27:52 +0100

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { Accordion, Navbar, } from 'react-bootstrap';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg='primary'>
-        <div className='container'>
-          <Navbar.Brand href='/'>Confusion</Navbar.Brand>
-        </div>
-      </Navbar>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -28,32 +24,7 @@ function App() {
       </header>
 
       <main>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-              est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-              est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <Menu></Menu>
       </main>
     </div>
   );
